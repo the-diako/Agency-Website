e2766365a03a4d00398e27836944868a85b9ac86
jacky boy <hello.1234.1994@gmail.com>
2025-01-04 04:30:48 +0330

    Created the constant file and fixed the assets file.

---
+++ src/constants/index.jsx
@@ -0,0 +1,176 @@
+import {
+  client01,
+  client02,
+  client03,
+  client04,
+  client05,
+  client06,
+  client07,
+  services01,
+  services02,
+  services03,
+  featuers01,
+  featuers02,
+  featuers03,
+  featuers04,
+} from "../assets";
+
+export const NavbarLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "featuers",
+    title: "Features",
+  },
+  {
+    id: "community",
+    title: "Community",
+  },
+  {
+    id: "blog",
+    title: "Blog",
+  },
+  {
+    id: "pricing",
+    title: "Pricing",
+  },
+];
+
+export const clients = [
+  {
+    id: 1,
+    icon: client01,
+  },
+  {
+    id: 2,
+    icon: client02,
+  },
+  {
+    id: 3,
+    icon: client03,
+  },
+  {
+    id: 4,
+    icon: client04,
+  },
+  {
+    id: 5,
+    icon: client05,
+  },
+  {
+    id: 6,
+    icon: client06,
+  },
+  {
+    id: 7,
+    icon: client07,
+  },
+];
+
+export const services = [
+  {
+    id: 1,
+    title: "Membership Organisations",
+    content:
+      "Our membership management software provides full automation of membership renewals and payments",
+    icon: services01,
+  },
+  {
+    id: 2,
+    title: "National Associations",
+    content:
+      "Our membership management software provides full automation of membership renewals and paymentsb",
+    icon: services02,
+  },
+  {
+    id: 3,
+    title: "Clubs And Groups",
+    content:
+      "Our membership management software provides full automation of membership renewals and paymentsh",
+    icon: services03,
+  },
+];
+
+export const features = [
+  {
+    id: 1,
+    title: "2,245,341",
+    content: "Members",
+    icon: featuers01,
+  },
+  {
+    id: 2,
+    title: "46,328",
+    content: "Clubs",
+    icon: featuers02,
+  },
+  {
+    id: 3,
+    title: "828,867",
+    content: "Event Bookings",
+    icon: featuers03,
+  },
+  {
+    id: 4,
+    title: "1,926,436",
+    content: "Payments",
+    icon: featuers04,
+  },
+];
+
+export const footerLinks = [
+  {
+    id: "header-1",
+    title: "Company",
+    links: [
+      {
+        id: 1,
+        ttile: "About us",
+      },
+      {
+        id: 2,
+        ttile: "Blog",
+      },
+      {
+        id: 3,
+        ttile: "Contact us",
+      },
+      {
+        id: 4,
+        ttile: "Pricing",
+      },
+      {
+        id: 5,
+        ttile: "Testimonials",
+      },
+    ],
+  },
+  {
+    id: "header-2",
+    title: "Support",
+    link: [
+      {
+        id: 1,
+        title: "Help center",
+      },
+      {
+        id: 2,
+        title: "Terms of service",
+      },
+      {
+        id: 3,
+        title: "Legal",
+      },
+      {
+        id: 4,
+        title: "Privacy policy",
+      },
+      {
+        id: 5,
+        title: "Status",
+      },
+    ],
+  },
+];
