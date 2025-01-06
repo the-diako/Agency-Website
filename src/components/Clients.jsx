3b681d346300ab2357da9bcbf6990f0e859e6f49
jacky boy <hello.1234.1994@gmail.com>
2025-01-06 20:54:29 +0330

    Client Section done.

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { clients } from "../constants";
 
 const Clients = () => {
-  return <div>Clients</div>;
+  return (
+    <div className="w-full flex flex-col items-center justify-center space-y-5 text-center mt-10 mb-5 ">
+      <div>
+        <h4 className="text-gray-700 font-semibold font-Roboto text-3xl md:text-4xl lg:text-5xl mb-4">
+          Our Clients
+        </h4>
+        <p className="text-gray-600 font-Roboto text-sm">
+          We have been working with some Fortune 500+ clients
+        </p>
+      </div>
+
+      <div className="w-full">
+        <ul className="w-full flex items-center justify-evenly">
+          {clients.map((items) => {
+            return (
+              <li key={items.id}>
+                <img src={items.icon} alt="clients" className="w-3/4" />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Clients;
