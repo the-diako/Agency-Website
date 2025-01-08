999a7aba40c6bbdcd4e8c6a8fd737dc40fcfabb0
jacky boy <hello.1234.1994@gmail.com>
2025-01-08 12:22:13 +0330

    Customer section done.

--- src/components/Customer.jsx
+++ src/components/Customer.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Button from "./Button";
 
 const Customer = () => {
-  return <div>Customer</div>;
+  return (
+    <div className="mt-10 py-28 w-full flex flex-col items-center justify-center text-center gap-6">
+      <h1
+        className="font-bold font-Roboto text-[#4D4D4D] 
+        text-[30px] 
+        md:text-[45px] 
+        lg:text-[55px]
+        xl:text-[85px]
+      "
+      >
+        Pellentesque suscipit
+        <br />
+        fringilla libero eu.
+      </h1>
+      <Button title="Get a Demo" />
+    </div>
+  );
 };
 
 export default Customer;
